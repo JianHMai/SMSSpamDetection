fe708b9537de272e049a9569371614e336301674
JianHMai <42229566+JianHMai@users.noreply.github.com>
2021-01-06 19:39:28 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# SMS Spam Detection
+
+This is a machine learning project to predict if a text message is spam or not. <br/>
+Dataset is retrieved from https://www.kaggle.com/uciml/sms-spam-collection-dataset
+
+## Team Members
+- Jian H Mai
+- Brian Thai
+
+## Algorithms Utilized
+- Porter Stemmer 
+
+## Libraries Utilized
+- NLTK
+- Csv
+- String 
---
+++ train.py
@@ -0,0 +1,28 @@
+import csv
+import sklearn.metrics
+import pickle
+from sklearn.model_selection import train_test_split
+
+def get_data():
+    
+    # Corpus
+    x = []
+    y = []
+
+    # Open CSV file
+    with open('processed.csv') as csvfile:
+        csvreader = csv.reader(csvfile, delimiter = ",")
+        # Goes through data in each row
+        for row in csvreader:
+            # Add message to list
+            x.append(row[1])
+            # Add classification to list spam = 1, ham = 0
+            if row[0] == 'spam':
+                y.append(1)
+            else: y.append(0)
+    # Seperate training data with 80% and test data with 20% of dataset
+    X_train, X_test, y_train, y_test = train_test_split(x,y, test_size=0.20)
+    return X_train, X_test, y_train, y_test  
+
+if __name__ == '__main__':
+    x_train, x_test, y_train, y_test = get_data()
