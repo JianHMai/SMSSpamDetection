479a0d74fc7a038b31248f06e41d0dbf7350caba
JianHMai <42229566+JianHMai@users.noreply.github.com>
2021-01-23 15:45:20 -0500

    Update file structure

--- train.py
+++ train.py
@@ -3,8 +3,8 @@ import sklearn.metrics
 import pickle
 from sklearn.model_selection import train_test_split
 
+# Retrieve data
 def get_data():
-    
     # Corpus
     x = []
     y = []
@@ -22,7 +22,11 @@ def get_data():
             else: y.append(0)
     # Seperate training data with 80% and test data with 20% of dataset
     X_train, X_test, y_train, y_test = train_test_split(x,y, test_size=0.20)
-    return X_train, X_test, y_train, y_test  
+    return X_train, X_test, y_train, y_test
+
+# Train model
+#def train():
+    # Used to vectorize words
 
 if __name__ == '__main__':
     x_train, x_test, y_train, y_test = get_data()
