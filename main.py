605de529c08a1f67fd8c40a9b856044ee0f61d72
JianHMai <42229566+JianHMai@users.noreply.github.com>
2021-01-24 02:04:58 -0500

    Add KNN

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from SMS import preprocess
+from train import get_data
+from train import train
+from train import validate_model
 import re
 import string
 import io
@@ -12,12 +15,18 @@ with open('dataset.csv') as f:
     with open('processed.csv','w',encoding='UTF-8') as new_file:
         for row in reader:
             for messages in row[1:-3]:
+                # Keep only ASCII letters and numbers
                 re.sub('[^a-zA-Z0-9]+', '', messages)
+                # Remove all non alphabetic letters 
+                encoded_string = messages.encode("ascii", "ignore")
+                messages = encoded_string.decode()
                 text = preprocess(word_tokenize(messages))
             for classification in row[:1]:
                 classify = classification
             new_file.write(classify + "," + text + "\n")
     new_file.close()
 f.close()
-    
-    #x_train, x_test, y_train, y_test = get_data()
+
+X_train, X_test, y_train, y_test = get_data()
+model = train(X_train, y_train)
+validate_model(model, X_test, y_test)
--- train.py
+++ train.py
@@ -2,6 +2,10 @@ import csv
 import sklearn.metrics
 import pickle
 from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.pipeline import Pipeline
+import sklearn.metrics
 
 # Retrieve data
 def get_data():
@@ -25,8 +29,22 @@ def get_data():
     return X_train, X_test, y_train, y_test
 
 # Train model
-#def train():
+def train(X_train, y_train):
     # Used to vectorize words
+    vectorizer = CountVectorizer()
+    neigh = KNeighborsClassifier(n_neighbors=1)
 
-if __name__ == '__main__':
-    x_train, x_test, y_train, y_test = get_data()
+    # Used to build a composite estimator using vectorizer and SVM 
+    model = Pipeline([('vectorizer', vectorizer), ('neigh', neigh)])
+    model.fit(X_train,y_train)
+
+    # Save model 
+    pickle.dump(model, open('model.sav', 'wb'))
+    return model
+
+# Used to validate model accuracy
+def validate_model(model, X_test, y_test):
+    # Use X training data to predict using the model
+    y_predict = model.predict(X_test)
+    # Used to measure accuracy of model
+    print(sklearn.metrics.classification_report(y_test, y_predict))
